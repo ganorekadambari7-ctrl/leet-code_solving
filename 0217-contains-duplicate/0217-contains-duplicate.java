d2962807ac76dc4b1b1e1a06e73b7d7fd35de442
ganorekadambari7-ctrl <ganorekadambari7@gmail.com>
2026-07-15 22:42:43 +0530

    Time: 24 ms (25.8%), Space: 81.8 MB (77.7%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        Arrays.sort(nums);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
