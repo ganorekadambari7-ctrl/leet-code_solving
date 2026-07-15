9fe9dcde316f1a1a91c62f38e6fc78d99b903893
ganorekadambari7-ctrl <ganorekadambari7@gmail.com>
2026-07-15 22:34:19 +0530

    Time: 0 ms (100%), Space: 43.2 MB (79.74%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n=digits.length;
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]==9){
+                digits[i]=0;
+            }
+            else{
+                digits[i]++;
+                return digits;
+            }
+        }
+        int ans[]=new int[n+1];
+        ans[0]=1;
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
