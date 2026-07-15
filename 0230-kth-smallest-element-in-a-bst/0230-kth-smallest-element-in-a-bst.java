62b80a1bd449860c34f7f75e6805905b85552792
ganorekadambari7-ctrl <ganorekadambari7@gmail.com>
2026-07-15 22:45:33 +0530

    Time: 0 ms (100%), Space: 46.9 MB (32.8%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count=0;
+    int ans=-1;
+   
+    public int kthSmallest(TreeNode root, int k) {
+        if(root.left!=null){
+            kthSmallest(root.left,k);
+        }
+        count++;
+        if(k==count){
+            ans=root.val;
+            return ans;
+        }
+        if(root.right!=null){
+            kthSmallest(root.right,k);
+        }
+        return ans;
+        
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
