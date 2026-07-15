b047bc942b1f042d6302a48903262b8a9246960b
ganorekadambari7-ctrl <ganorekadambari7@gmail.com>
2026-07-15 22:19:50 +0530

    Time: 3 ms (99.9%), Space: 46.2 MB (90.49%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String intToRoman(int num) {
+        final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        final String[] symbols = {"M", "CM", "D",  "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < values.length; ++i) {
+            if (num == 0)
+                break;
+            while (num >= values[i]) {
+                sb.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+
+        return sb.toString();        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
