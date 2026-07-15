f7cc2f373f3b6f46e3bba2e91f97e7b509be4ddd
ganorekadambari7-ctrl <ganorekadambari7@gmail.com>
2026-07-15 22:30:46 +0530

    Time: 2 ms (99.83%), Space: 43.9 MB (25.31%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        int m = num1.length();
+        int n = num2.length();
+        int[] res = new int[m + n];
+
+        for (int i = m - 1; i >= 0; i--) {
+            int d1 = num1.charAt(i) - '0';
+
+            for (int j = n - 1; j >= 0; j--) {
+                int d2 = num2.charAt(j) - '0';
+                int mul = d1 * d2;
+
+                int p1 = i + j;
+                int p2 = i + j + 1;
+
+                int sum = mul + res[p2];
+
+                res[p2] = sum % 10;
+                res[p1] += sum / 10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int num : res) {
+            if (!(sb.length() == 0 && num == 0)) {
+                sb.append(num);
+            }
+        }
+
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
