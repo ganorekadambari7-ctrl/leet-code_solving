2c62d4cab82737a750bb0ed31c012d138b9ad134
ganorekadambari7-ctrl <ganorekadambari7@gmail.com>
2026-07-17 15:32:47 +0530

    Time: 1 ms (98.05%), Space: 45.2 MB (15.83%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+
+        int time = 0;
+
+        for (int i = 1; i < points.length; i++) {
+            int x = Math.abs(points[i][0] - points[i - 1][0]);
+            int y = Math.abs(points[i][1] - points[i - 1][1]);
+
+            time += Math.max(x, y);
+        }
+
+        return time;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
